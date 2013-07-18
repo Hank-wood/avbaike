327ca9c88738db419ff34cdc1dfbeca673d98e36
scige <panf06@gmail.com>
2013-07-18 14:11:56 +0800

    add list interface

--- app/controllers/girls_controller.rb
+++ app/controllers/girls_controller.rb
@@ -6,4 +6,15 @@ class GirlsController < ApplicationController
       format.json { render json: @girl }
     end
   end
+
+  def list
+    id_begin = params[:begin]
+    id_end   = params[:end]
+    @girls = Girl.where("id>=#{id_begin} and id<=#{id_end}")
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @girls }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Avbaike::Application.routes.draw do
-  resources :girls
+  resources :girls do
+    get :list, :on => :collection
+  end
 
   root :to => 'home#index'
 end
